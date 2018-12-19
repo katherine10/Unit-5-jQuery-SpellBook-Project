57f95eb9caad459f939066bbf712034edf819d28
katherine10 <43441924+katherine10@users.noreply.github.com>
2018-12-18 20:07:22 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$("#toggle").click(function(){
+    $(".pendant").toggle ();
+});
+
+
+
+//Spell #2
+
+$("#fadeToggle").click(function(){
+    $(".magic").fadeToggle ();
+});
+
+
+
+//Spell #3
+
+$("#slideToggle").click(function(){
+   $(".cape").slideToggle ();
+});
+
+
+
